8cd6a0d597467c8c4a9aabb64fb8326f404d0f00
wisut hantanong <wizzup@gmail.com>
2017-08-27 17:02:38 +0700

    make make-container.sh callable anywhere

--- build/make-container.sh
+++ build/make-container.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 # This builds the container
-docker build . -t s2client-api
+IMGNAME=s2client-api
+SCRIPT_PATH=${0%/*}
+
+echo ${IMGNAME} docker image building using ${SCRIPT_PATH}/Dockerfile
+docker build ${SCRIPT_PATH} -t ${IMGNAME}
