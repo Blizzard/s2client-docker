53a864e9025d676674c7af0eab6240bf6abd0a0c
Jacob Repp <jrepp@blizzard.com>
2017-08-28 09:27:37 -0700

    Adding comment to docker-cleanup.sh

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+#
+# This cleans up runtime images that have exited and prevents accumulating
+# a lot of docker image bloat on your local system
+#
 docker rm $(docker ps -qaf status=exited)
