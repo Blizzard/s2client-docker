7a0918f19446347e9952451aa26582f5fb2d198d
Jacob Repp <jrepp@blizzard.com>
2017-08-29 15:12:13 -0700

    Fixing missing line continuations in game/run-interactive.sh

--- game/run-interactive.sh
+++ game/run-interactive.sh
@@ -3,6 +3,6 @@
 # Run an interactive container with the build (see build/all.sh)
 docker run \
     --entrypoint="/bin/bash" \
-    -v code:/code
-    -v build:/build 
+    -v code:/code \
+    -v build:/build  \
     -it s2client-game
