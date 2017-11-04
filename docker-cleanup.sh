9229e592b166eaf8d771a16e7f950203afa39a6d
Jacob Repp <jacobrepp@gmail.com>
2017-11-04 10:53:25 -0700

    Updating cleanup script

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -2,19 +2,19 @@
 
 # Cleanup stopped containers and then remove unused images
 stale_containers=$(docker ps --filter status=exited --quiet)
-# echo "stale containers: ${stale_containers}"
+echo Cleaning stale containers
 if [ -n "$stale_containers" ]; then
 	docker rm $stale_containers
 fi
 
 stale_images=$(docker images --filter dangling=true --quiet)
-# echo "stale images: ${stale_images}"
+echo Cleaning stale images
 if [ -n "$stale_images" ]; then
 	docker rmi $stale_images
 fi
 
 stale_volumes=$(docker volume ls -qf dangling=true)
-echo "stale volumes: ${stale_volumes}"
+echo Cleaning stale volumes
 if [ -n "$stale_volumes" ]; then
 	docker volume rm $stale_volumes
 fi
